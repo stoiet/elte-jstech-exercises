0f399b1089bb28c08487f1770b859fe6f4690b40
stoiet <to.stoiet@gmail.com>
2017-11-16 17:15:52 +0100

    feat(player-manager): configurate webpack

--- player-manager/client/bootstrap.js
+++ player-manager/client/bootstrap.js
@@ -1,5 +1,6 @@
 import { ApplicationComponent } from './components';
 import { render } from 'react-dom';
 import * as React from 'react';
+import 'core-js/shim';
 
 render(<ApplicationComponent />, document.getElementById('application'));
---
+++ player-manager/webpack.config.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const webpack = require('webpack');
+
+const config = {
+  context: __dirname,
+  entry: {
+    application: './client/bootstrap.js',
+    vendors: ['react', 'react-dom', 'tslib', 'core-js'],
+  },
+  devtool: 'source-map',
+  resolve: { extensions: ['.js', '.json', '.ts', '.tsx'] },
+  output: {
+    filename: '[name].js',
+    path: `${__dirname}/dist`,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loaders: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({ name: ['vendors'] }),
+  ],
+};
+
+module.exports = config;
