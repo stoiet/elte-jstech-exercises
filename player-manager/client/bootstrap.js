ad898f13e8e239d82be10ad2daefe57e451fc616
stoiet <to.stoiet@gmail.com>
2017-11-16 15:23:46 +0100

    feat(player-manager): introduce

---
+++ player-manager/client/bootstrap.js
@@ -0,0 +1,5 @@
+import { ApplicationComponent } from './components';
+import { render } from 'react-dom';
+import * as React from 'react';
+
+render(<ApplicationComponent />, document.getElementById('application'));
---
+++ player-manager/client/components/application/index.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export class ApplicationComponent extends React.Component {
+
+  render() {
+    return <span>{ 'Hello World!' }</span>;
+  }
+
+}
