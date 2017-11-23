e4b4bfa4a7fce96ba5dc404943b2d1740c70975d
stoiet <to.stoiet@gmail.com>
2017-11-23 05:12:57 +0100

    chore(player-modules/webpack): specify configuration

--- player-manager/webpack.config.js
+++ player-manager/webpack.config.js
@@ -1,18 +1,20 @@
 'use strict';
 
+const { resolve } = require('path');
 const webpack = require('webpack');
 
 const config = {
-  context: __dirname,
+  context: resolve(__dirname, './'),
   entry: {
     application: './client/bootstrap.js',
     vendors: ['react', 'react-dom', 'prop-types', 'core-js', 'regenerator-runtime'],
   },
-  devtool: 'source-map',
-  resolve: { extensions: ['.js', '.json', '.ts', '.tsx'] },
   output: {
     filename: '[name].js',
-    path: `${__dirname}/dist`,
+    path: resolve(__dirname, './dist'),
+    pathinfo: true,
+    publicPath: 'http://localhost:9100/dist',
+    sourceMapFilename: '[name].map',
   },
   module: {
     rules: [
@@ -23,9 +25,17 @@ const config = {
       },
     ],
   },
+  resolve: { modules: [resolve(__dirname, './node_modules')] },
   plugins: [
     new webpack.optimize.CommonsChunkPlugin({ name: ['vendors'] }),
   ],
+  devtool: 'source-map',
+  target: 'web',
+  watchOptions: {
+    aggregateTimeout: 500,
+    ignored: /node_modules/,
+    poll: 1000,
+  },
 };
 
 module.exports = config;
