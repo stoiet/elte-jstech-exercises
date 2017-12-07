9370cf871d4c028b36927773abfcb0ac3a5ccd95
stoiet <to.stoiet@gmail.com>
2017-12-07 15:03:59 +0100

    feat(player-manager/player-edit): add template

--- player-manager/client/components/player-edit/index.js
+++ player-manager/client/components/player-edit/index.js
@@ -5,17 +5,37 @@ export class PlayerEditComponent extends React.Component {
 
   render() {
     return (
-      <div>
+      <form>
+        <div className={ 'form-group' }>
+          <label htmlFor={ 'playerName' }>Player Name</label>
+          <input
+            id={ 'playerName' }
+            type={ 'text' }
+            className={ 'form-control input-sm' }
+            value={ this.props.player.name }
+          />
+        </div>
+        <div className={ 'form-group' }>
+          <label htmlFor={ 'playerStatus' }>Player Status</label>
+          <select id={ 'playerStatus' } className={ 'form-control input-sm' } value={ this.props.player.status } >
+            <option value={ 'online' }>Online</option>
+            <option value={ 'offline' }>Offline</option>
+            <option value={ 'freezed' }>Freezed</option>
+            <option value={ 'banned' }>Banned</option>
+            <option value={ 'kicked' }>Kicked</option>
+          </select>
+        </div>
         <button
           type={ 'button' }
-          className={ 'btn btn-success btn-sm' }
+          className={ 'btn btn-success btn-xs' }
         >Apply</button>
+        { ' ' }
         <button
           type={ 'button' }
-          className={ 'btn btn-default btn-sm' }
+          className={ 'btn btn-default btn-xs' }
           onClick={ () => this.handleCancel() }
         >Cancel</button>
-      </div>
+      </form>
     );
   }
 
